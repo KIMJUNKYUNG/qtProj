26f3530eddb7e488a63ccf3da038a2dfa1ba444d
KIMJUNKYUNG <kimkj112@naver.com>
2024-11-10 14:48:02 +0800

    UI Done

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,7 +8,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    btnOptions <<"pushButton_1" <<"pushButton_2" <<"pushButton_3" <<"pushButton_4"
+    btnOptions <<"pushButton_0"<<"pushButton_1" <<"pushButton_2" <<"pushButton_3" <<"pushButton_4"
                <<"pushButton_5" <<"pushButton_6" <<"pushButton_7" <<"pushButton_8" <<"pushButton_9";
 
     connect(ui->pushButton_1, &QPushButton::released, this, &MainWindow::slotPushedButton);
@@ -31,30 +31,33 @@ void MainWindow::slotPushedButton()
     QString curText = ui->lineEdit->text();
     switch (btnOptions.indexOf(senderName)) {
     case 0:
-        ui->lineEdit->setText(curText + "1");
+        ui->lineEdit->setText(curText + "0");
         break;
     case 1:
-        ui->lineEdit->setText(curText + "2");
+        ui->lineEdit->setText(curText + "1");
         break;
     case 2:
-        ui->lineEdit->setText(curText + "3");
+        ui->lineEdit->setText(curText + "2");
         break;
     case 3:
-        ui->lineEdit->setText(curText + "4");
+        ui->lineEdit->setText(curText + "3");
         break;
     case 4:
-        ui->lineEdit->setText(curText + "5");
+        ui->lineEdit->setText(curText + "4");
         break;
     case 5:
-        ui->lineEdit->setText(curText + "6");
+        ui->lineEdit->setText(curText + "5");
         break;
     case 6:
-        ui->lineEdit->setText(curText + "7");
+        ui->lineEdit->setText(curText + "6");
         break;
     case 7:
-        ui->lineEdit->setText(curText + "8");
+        ui->lineEdit->setText(curText + "7");
         break;
     case 8:
+        ui->lineEdit->setText(curText + "8");
+        break;
+    case 9:
         ui->lineEdit->setText(curText + "9");
         break;
     default:
